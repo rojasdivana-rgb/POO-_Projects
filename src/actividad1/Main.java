ce35487b55c7698fba9d9a4a718a3476b133bf78
rojasdivana-rgb <rojasdivana@gmail.com>
2026-01-17 20:52:39 -0600

    Add: Estructura actividad1 con todas las clases Java

---
+++ src/actividad1/Main.java
@@ -0,0 +1,10 @@
+package actividad1;
+
+import actividad1.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI interfaz = new CLI();
+        interfaz.iniciar();
+    }
+}
