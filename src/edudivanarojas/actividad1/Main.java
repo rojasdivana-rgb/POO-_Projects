1e4146dfa753eff2397f116038001d43e6b58a21
rojasdivana-rgb <rojasdivana@gmail.com>
2026-01-19 16:00:20 -0600

    Restructure: Reorganizar proyecto a estructura estándar src/ - Corrige ConfigError de VS Code

---
+++ src/edudivanarojas/actividad1/Main.java
@@ -0,0 +1,11 @@
+package edu.divana.rojas.actividad1;
+
+import edu.divana.rojas.actividad1.ui.CLI;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI interfaz = new CLI();
+        interfaz.iniciar();
+    }
+}
+
